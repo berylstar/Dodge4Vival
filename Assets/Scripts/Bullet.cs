c62c523c242fb31b705c8b4e9c0f64327134d756
minsang <minsang0610@naver.com>
2023-09-08 19:05:14 +0900

    [ADD] Animation (-minsang)

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BulletType
+{
+    players,
+    monsters,
+}
+
 public class Bullet : MonoBehaviour
 {
+    [Header("Status")]
+    public BulletType type;
+    public int damage;
     public float speed;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(this.gameObject);
+        if (collision.CompareTag("Monster") && type == BulletType.players)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // 발사체는 일직선으로 발사
