e19b959e1b3466941b431a7eeb6f54fdeb45a630
장성림 <hoobhoob214@gmail.com>
2023-09-08 16:09:38 +0900

    [UPDATE] 몬스터 랜덤위치 생성 임시로 완성(-성림)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private MonsterSpawnController _monsterSpawnController;
+    [SerializeField] private GameObject _player;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _monsterSpawnController = GetComponent<MonsterSpawnController>();
+        _monsterSpawnController.SetFullCount(10);
+        _monsterSpawnController.SetSpawnPosition(new Vector2(_player.transform.position.x, _player.transform.position.y));
     }
 
+    void Start()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
