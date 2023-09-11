9f8220e56287a42d0b08ddc1a2d947dfb2240d45
장성림 <hoobhoob214@gmail.com>
2023-09-11 17:44:14 +0900

    [ADD] 무적 이벤트 파일만 추가 (-성림)

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -12,6 +12,7 @@ public class EventManager : MonoBehaviour
     public UnityEvent PlayerDieEvent;
     public UnityEvent PlayerHealingEvent;
     public UnityEvent PlayerLowHPEvent;
+    public UnityEvent PlayerInvencibleEvent;
     public UnityEvent MonsterAllDestroyed;
     public UnityEvent MonsterDieEvent;
 
