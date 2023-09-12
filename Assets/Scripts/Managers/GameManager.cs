ad512cc386f71029f388e429b492125c46a3d8a5
minsang <minsang0610@naver.com>
2023-09-12 20:09:01 +0900

    [UPDATE] GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -9,28 +10,41 @@ public class GameManager : MonoBehaviour
 
     public Player player;
 
+    [Header("UI")]
     [SerializeField] private GameObject _endPanel;
     [SerializeField] private TMP_Text _TimeText;
     [SerializeField] private TMP_Text _bestScoreText;
     [SerializeField] private TMP_Text _thisScoreText;
 
-    [SerializeField] private MonsterSpawnController _monsterSpawnController;
+    [Header("MonsterSpawn")]
+    public IntVariable MonsterFullCount;
+    public FloatVariable SpawnCooldownTime;
+    public Transform spawnHolder;
 
-    private float _inTime = 0f;
+    public List<GameObject> monsters1 = new List<GameObject>();
+    public List<GameObject> monsters2 = new List<GameObject>();
+    public List<GameObject> monsters3 = new List<GameObject>();
+    public List<GameObject> monsters4 = new List<GameObject>();
+
+    [SerializeField] private List<GameObject> spawnableMonsters = new List<GameObject>();
 
     [Header("Variables")]
     public FloatVariable GameEndTime;
+
+    private float _inTime = 0f;
 
     void Awake()
     {
         I = this;
 
         Time.timeScale = 1.0f;
-    }
 
-    void Start()
-    {
-        _monsterSpawnController.SetSpawnPosition(player.transform.position);
+        foreach (GameObject mon in monsters1)
+        {
+            spawnableMonsters.Add(mon);
+        }
+
+        StartCoroutine(Spawn(SpawnCooldownTime.f));
     }
 
     void Update()
@@ -49,7 +63,6 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         Time.timeScale = 0.0f;
-        _monsterSpawnController.SetCanSpawn(false);
         _bestScoreText.text = _TimeText.text;
         _thisScoreText.text = _TimeText.text;
         _endPanel.SetActive(true);
@@ -58,5 +71,41 @@ public class GameManager : MonoBehaviour
     public void Retry()
     {
         SceneManager.LoadScene("StartScene");
+    }
+
+    private Vector2 RandomSpawnPosition()
+    {
+        float x = player.transform.position.x + Random.Range(-8.0f, 8.0f);
+        float y = player.transform.position.y + Random.Range(-5.0f, 5.0f);
+
+        x = Mathf.Max(Mathf.Min(x, 54), -54);
+        y = Mathf.Max(Mathf.Min(y, 43), -43);
+
+        return new Vector2(x, y);
+    }
+
+    private void AddMonster()
+    {
+
+    }
+
+    private IEnumerator Spawn(float spawnCooldownTime)
+    {
+        while (true)
+        {
+            Debug.Log(_inTime);
+
+            Instantiate(spawnableMonsters[Random.Range(0, spawnableMonsters.Count)], RandomSpawnPosition(), Quaternion.identity, spawnHolder);
+
+            yield return new WaitForSeconds(spawnCooldownTime);
+        }        
+    }
+
+    public void OnDestroyAllMonster()
+    {
+        foreach (Transform child in spawnHolder)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
